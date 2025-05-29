c26e3be1f424e47a1ba050812771a9ffc29097c7
carlosJavierRodriguez <carlosjavi1887@gmail.com>
2025-05-29 06:44:43 -0500

    Se creo el proyecto de Spring Boot para el backend de correos-pacientes

---
+++ backend/correos-pacientes/src/main/java/com/example/correos_pacientes/CorreosPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.example.correos_pacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreosPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreosPacientesApplication.class, args);
+	}
+
+}
